d04299df492b268aa0627d07c40395d830400d3c
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-23 21:46:56 +0600

    Taking string as input.

---
+++ inputString.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    
+    // Taking string as input
+    cin >> name;
+    
+    cout << "Entered name: " << name;
+    return 0;
+}
